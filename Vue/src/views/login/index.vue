656bee40bec61bc9c1fab1a8892af6af9a487b6b
FeiBam <feibam19690104@gmail.com>
2021-07-16 09:18:50 +0900

    完成登录界面 同步仓库

--- Vue/src/views/login/index.vue
+++ Vue/src/views/login/index.vue
@@ -1,75 +1,56 @@
 <template>
-  <div class="login-container">
-    <el-form ref="loginForm" :model="loginForm" :rules="loginRules" class="login-form" autocomplete="on" label-position="left">
-
-      <div class="title-container">
-        <h3 class="title">Login Form</h3>
-      </div>
-
-      <el-form-item prop="username">
-        <span class="svg-container">
-          <svg-icon icon-class="user" />
-        </span>
-        <el-input
-          ref="username"
-          v-model="loginForm.username"
-          placeholder="Username"
-          name="username"
-          type="text"
-          tabindex="1"
-          autocomplete="on"
-        />
-      </el-form-item>
-
-      <el-tooltip v-model="capsTooltip" content="Caps lock is On" placement="right" manual>
-        <el-form-item prop="password">
-          <span class="svg-container">
-            <svg-icon icon-class="password" />
-          </span>
-          <el-input
-            :key="passwordType"
-            ref="password"
-            v-model="loginForm.password"
-            :type="passwordType"
-            placeholder="Password"
-            name="password"
-            tabindex="2"
-            autocomplete="on"
-            @keyup.native="checkCapslock"
-            @blur="capsTooltip = false"
-            @keyup.enter.native="handleLogin"
-          />
-          <span class="show-pwd" @click="showPwd">
-            <svg-icon :icon-class="passwordType === 'password' ? 'eye' : 'eye-open'" />
-          </span>
-        </el-form-item>
-      </el-tooltip>
-
-      <el-button :loading="loading" type="primary" style="width:100%;margin-bottom:30px;" @click.native.prevent="handleLogin">Login</el-button>
-
-      <div style="position:relative">
-        <div class="tips">
-          <span>Username : admin</span>
-          <span>Password : any</span>
+  <div>
+    <el-row class="login-container" type="flex">
+      <el-col :xs="0" :sm="12" :md="16" :lg="18">
+        <el-image
+          style="width: 100%; height: 100%"
+          :fit="'cover'"
+          :src="require('@/assets/login_background.png')"
+        ></el-image>
+      </el-col>
+      <el-col style="padding: 0 20px" :xs="24" :sm="12" :md="8" :lg="6">
+        <div style="height: 33%;padding-top: 20px" class="login-form-icon-container login-form-container">
+          <el-image
+            style="width: 150px;height: 150px;"
+            :src="require('@/assets/icon.jpg')"
+            :fit="'cover'"
+          >
+          </el-image>
+          <div style="margin-top: 7px;font-size: x-large">灰风字幕组</div>
+          <div style="margin-top: 12px">工作平台</div>
         </div>
-        <div class="tips">
-          <span style="margin-right:18px;">Username : editor</span>
-          <span>Password : any</span>
+        <div style="height: 66%" class="login-form login-form-container">
+          <el-form>
+            <el-form-item>
+              <el-input placeholder="账户名"></el-input>
+            </el-form-item>
+            <el-form-item>
+              <el-input placeholder="密码"></el-input>
+            </el-form-item>
+            <el-form-item>
+              <div style="display: flex;justify-content: space-between">
+                <el-checkbox
+                  label="30天内记住密码"
+                  name="type"
+                ></el-checkbox>
+                <a
+                  @click="$notify({
+                      title: '丢失密码？',
+                      message: '请找群内管理员重置密码',
+                })"
+                  style="color: rgb(94 179 209)">忘记密码？</a>
+              </div>
+            </el-form-item>
+            <el-form-item>
+              <el-button
+                style="width: 100%;height: 46px"
+                type="primary"
+              >登录</el-button>
+            </el-form-item>
+          </el-form>
         </div>
-
-        <el-button class="thirdparty-button" type="primary" @click="showDialog=true">
-          Or connect with
-        </el-button>
-      </div>
-    </el-form>
-
-    <el-dialog title="Or connect with" :visible.sync="showDialog">
-      Can not be simulated on local, so please combine you own business simulation! ! !
-      <br>
-      <br>
-      <br>
-      <social-sign />
-    </el-dialog>
+      </el-col>
+    </el-row>
   </div>
 </template>
 
@@ -200,125 +181,24 @@ export default {
 }
 </script>
 
-<style lang="scss">
-/* 修复input 背景不协调 和光标变色 */
-/* Detail see https://github.com/PanJiaChen/vue-element-admin/pull/927 */
-
-$bg:#283443;
-$light_gray:#fff;
-$cursor: #fff;
-
-@supports (-webkit-mask: none) and (not (cater-color: $cursor)) {
-  .login-container .el-input input {
-    color: $cursor;
-  }
+<style scoped>
+.login-container{
+  min-height: 100vh;
+  width: 100vw;
 }
-
-/* reset element-ui css */
-.login-container {
-  .el-input {
-    display: inline-block;
-    height: 47px;
-    width: 85%;
-
-    input {
-      background: transparent;
-      border: 0px;
-      -webkit-appearance: none;
-      border-radius: 0px;
-      padding: 12px 5px 12px 15px;
-      color: $light_gray;
-      height: 47px;
-      caret-color: $cursor;
-
-      &:-webkit-autofill {
-        box-shadow: 0 0 0px 1000px $bg inset !important;
-        -webkit-text-fill-color: $cursor !important;
-      }
-    }
-  }
-
-  .el-form-item {
-    border: 1px solid rgba(255, 255, 255, 0.1);
-    background: rgba(0, 0, 0, 0.1);
-    border-radius: 5px;
-    color: #454545;
-  }
+.login-container > div{
+  height: 100vh;
+}
+.login-form-icon-container{
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+}
+.login-form{
+  display: flex;
+  flex-direction: column;
+  padding-top: 72px;
 }
 </style>
 
-<style lang="scss" scoped>
-$bg:#2d3a4b;
-$dark_gray:#889aa4;
-$light_gray:#eee;
-
-.login-container {
-  min-height: 100%;
-  width: 100%;
-  background-color: $bg;
-  overflow: hidden;
-
-  .login-form {
-    position: relative;
-    width: 520px;
-    max-width: 100%;
-    padding: 160px 35px 0;
-    margin: 0 auto;
-    overflow: hidden;
-  }
-
-  .tips {
-    font-size: 14px;
-    color: #fff;
-    margin-bottom: 10px;
-
-    span {
-      &:first-of-type {
-        margin-right: 16px;
-      }
-    }
-  }
-
-  .svg-container {
-    padding: 6px 5px 6px 15px;
-    color: $dark_gray;
-    vertical-align: middle;
-    width: 30px;
-    display: inline-block;
-  }
-
-  .title-container {
-    position: relative;
-
-    .title {
-      font-size: 26px;
-      color: $light_gray;
-      margin: 0px auto 40px auto;
-      text-align: center;
-      font-weight: bold;
-    }
-  }
-
-  .show-pwd {
-    position: absolute;
-    right: 10px;
-    top: 7px;
-    font-size: 16px;
-    color: $dark_gray;
-    cursor: pointer;
-    user-select: none;
-  }
-
-  .thirdparty-button {
-    position: absolute;
-    right: 0;
-    bottom: 6px;
-  }
-
-  @media only screen and (max-width: 470px) {
-    .thirdparty-button {
-      display: none;
-    }
-  }
-}
-</style>
