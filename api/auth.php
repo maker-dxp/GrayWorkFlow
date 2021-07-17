444e12c51233dd615003f587fa5faaa762e7e6b1
CberYellowstone <administrator@mail.ystone.top>
2021-07-17 21:44:26 +0800

    剥离非核心代码

--- api/auth.php
+++ api/auth.php
@@ -2,70 +2,10 @@
 // error_reporting (E_ALL || ~E_NOTICE);
 ini_set("display_errors", "On");
 error_reporting(E_ALL | E_STRICT);
+define('IN_SYS', TRUE);
+require_once 'function.php';
 
 use Firebase\JWT\JWT;
-
-function is_get():bool{return $_SERVER['REQUEST_METHOD'] == 'GET' ? true : false;}
-function is_post():bool{return $_SERVER['REQUEST_METHOD'] == 'POST' ? true : false;}
-function _echo($i){echo $i;return true;}    //echo函数化
-
-function sendHttpStatus($code) {
-    static $_status = array(
-            // Informational 1xx
-            100 => 'Continue',
-            101 => 'Switching Protocols',
-            // Success 2xx
-            200 => 'OK',
-            201 => 'Created',
-            202 => 'Accepted',
-            203 => 'Non-Authoritative Information',
-            204 => 'No Content',
-            205 => 'Reset Content',
-            206 => 'Partial Content',
-            // Redirection 3xx
-            300 => 'Multiple Choices',
-            301 => 'Moved Permanently',
-            302 => 'Moved Temporarily ',  // 1.1
-            303 => 'See Other',
-            304 => 'Not Modified',
-            305 => 'Use Proxy',
-            // 306 is deprecated but reserved
-            307 => 'Temporary Redirect',
-            // Client Error 4xx
-            400 => 'Bad Request',
-            401 => 'Unauthorized',
-            402 => 'Payment Required',
-            403 => 'Forbidden',
-            404 => 'Not Found',
-            405 => 'Method Not Allowed',
-            406 => 'Not Acceptable',
-            407 => 'Proxy Authentication Required',
-            408 => 'Request Timeout',
-            409 => 'Conflict',
-            410 => 'Gone',
-            411 => 'Length Required',
-            412 => 'Precondition Failed',
-            413 => 'Request Entity Too Large',
-            414 => 'Request-URI Too Long',
-            415 => 'Unsupported Media Type',
-            416 => 'Requested Range Not Satisfiable',
-            417 => 'Expectation Failed',
-            // Server Error 5xx
-            500 => 'Internal Server Error',
-            501 => 'Not Implemented',
-            502 => 'Bad Gateway',
-            503 => 'Service Unavailable',
-            504 => 'Gateway Timeout',
-            505 => 'HTTP Version Not Supported',
-            509 => 'Bandwidth Limit Exceeded'
-    );
-    if(isset($_status[$code])) {
-        header('HTTP/1.1 '.$code.' '.$_status[$code]);
-        // 确保FastCGI模式下正常
-        header('Status:'.$code.' '.$_status[$code]);
-    }
-    return true;
-}
 
 function createToken($username,$userid,$usergroup='Undefined'){
     include_once 'Firebase/JWT/JWT.php';
@@ -217,6 +157,7 @@ case "createUser":
     break;
     default:
     echo('{"message":"无效的请求","code":400}');
+    sendHttpStatus(400);
 }
 
 ?>
