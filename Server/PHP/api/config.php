8d1a4f08503bf2dd254e8ec11c179aa376d3356b
CberYellowstone <administrator@mail.ystone.top>
2021-07-27 15:07:09 +0800

    fixnt语句

--- Server/PHP/api/config.php
+++ Server/PHP/api/config.php
@@ -11,7 +11,6 @@ define('WWWROOT_LENGTH', strlen(WWWROOT));
 //网站根目录,不包含'/',如为根目录则留空
 
 const APIROOT       =       '';
-define('APIROOT', strlen(APIROOT));
 //api目录相对路径,默认为api,可填入:api/不填
 
 ini_set('date.timezone', 'Asia/Shanghai');
